1b061d2973235d28711c9c1a1a36f13823eb3a4f
MTuongnxm <manhtuong011090@gmail.com>
2021-08-16 21:10:31 +0700

    chuyendoitiente

---
+++ js/my.js
@@ -0,0 +1,8 @@
+function btn() {
+    var b = +document.getElementById('btn').Value;
+    b.addeventlistener("click",function()){
+        var m = +document.getElementById('money').value;
+        var numUSD
+    })
+
+}
